d8bc8072178b29c1f94b2dfb9e2f868bc5305a33
Daria Trip <daria.trip001@gmail.com>
2021-04-03 04:05:36 +0300

    checked genders(to solve)

--- teamGender.js
+++ teamGender.js
@@ -1,20 +1,42 @@
-function noFemaleTerro()
+function noFemaleCT()
 {
-    var select = document.getElementById("select_gender");
-    var terroCheckBox = document.getElementById("terro");
-    var female = document.getElementById('female');
-    if (terroCheckBox.checked) {
-        select.removeChild(female);
-    }
-    else
-    {
+
+    // Get the checkbox
+    var terro = document.getElementById("terro");
+    //parent gender
+    var Select = document.getElementById("select_gender");
+    // Remove the female gender
+    var female_terro = document.getElementById("female");
+
+    if (terro.checked == true){
+        female_terro.remove();   
         
+    }
+    var x = document.getElementById("select_gender").childElementCount;
+    if(x==1)
+    {
+        //console.log(x);
+        withFemaleCT();
     }
 }
 
-// is not implemented correctly
+function withFemaleCT()
+{
+    var male_CT = document.getElementById("male");
+    if(male_CT.checked == true )
+    {
+        //sa readaugam female cand Counter-Terro e selectat
+        var female_add = document.createElement('option');
+        var node = document.createTextNode("Female");
+        female_add.appendChild(node);
+        Select.appendChild(female_add);
+    }   
+}
 
 
 
 
 
+function GenerateCharacter(){
+    console.log("POZA CARACTER");
+}
